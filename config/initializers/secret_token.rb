9beaff4e9e96a6e6ce3d59e1cb9f139e7ebaf2d8
Feifan Wang <feifanw@gmail.com>
2013-06-19 01:23:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buddyup::Application.config.secret_token = '3d197f5676c4cc55000d94add76775ecf0a8c10cb0bb09a1818482fd1b89a8424bd6c3310394df71d96eef9c964746729dd44caf02c6e4afe86ea77641bba462'
